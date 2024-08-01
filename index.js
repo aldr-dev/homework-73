0b30f00fa63949b82a38396aab5a173eae58106d
Gordus Alexander <aldrmiller@gmail.com>
2024-08-01 21:19:25 +0600

    объединил задания в один главный файл index.js. настроил nodemon. поправил README.md

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const Vigenere = require('caesar-salad').Vigenere;
-
 const app = express();
 const port = 8000;
-
 const password = 'password';
 
+app.get('/:message', (req, res) => {
+   return res.send(req.params.message);
+});
+
 app.get('/encode/:message', (req, res) => {
-    res.send(Vigenere.Cipher(password).crypt(req.params.message));
+  return res.send(Vigenere.Cipher(password).crypt(req.params.message));
 });
 
 app.get('/decode/:message', (req, res) => {
-    res.send(Vigenere.Decipher(password).crypt(req.params.message));
+   return res.send(Vigenere.Decipher(password).crypt(req.params.message));
 });
 
 app.listen(port, () => {
